4de0d2c556203c361dc2cb2e4acdb582ea4ed3eb
neojarvis <techadmin@iamneo.ai>
2024-02-06 08:09:01 +0000

    Auto-save-commit-06/02/2024--13:39:00

--- angularapp/src/app/components/edit-event/edit-event.component.ts
+++ angularapp/src/app/components/edit-event/edit-event.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { EventmanagementService } from 'src/app/services/eventmanagement.service';
 
 @Component({
   selector: 'app-edit-event',
@@ -7,9 +9,82 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditEventComponent implements OnInit {
 
-  constructor() { }
+  event: any = {};
+  photoImage="";
+  constructor(private route: ActivatedRoute, private eventManagementService: EventmanagementService, private router: Router) { }
 
-  ngOnInit(): void {
+  category = ['House', 'Apartment', 'Villa', 'Cabin', 'Condo', 'Other'];
+
+  ngOnInit() {
+    const eventId = this.route.snapshot.paramMap.get('id');
+    console.log('Event Ids to be edited', eventId);
+    this.getEventManagementById(eventId);
   }
+
+  getEventManagementById(id: string) {
+    this.eventManagementService.getEventManagementById(id).subscribe(
+      (response) => {
+        this.event = response;
+        console.log(response)
+      },
+      (error) => {
+        console.error('Error retrieving mobile', error);
+      }
+    );
+  }
+
+  updateEventManagement() {
+    this.event.coverImage=this.photoImage;
+    this.eventManagementService.updateEventManagement(this.event).subscribe(
+      (response) => {
+        console.log('Event updated successfully', response);
+        //navigate to seller dashboard
+        this.router.navigate(['/organiser-dashboard']);
+      },
+      (error) => {
+        console.error('Error updating mobile', error);
+      }
+    );
+ }
+
+ 
+ goBack(): void {
+  // Navigate to the dashboard or any desired route
+  this.router.navigate(['/organiser-dashboard']);
+}
+
+ handleFileChange(event: any): void {
+  const file = event.target.files[0];
+
+  if (file) {
+    this.convertFileToBase64(file).then(
+      (base64String) => {
+        this.photoImage=base64String
+      },
+      (error) => {
+        console.error('Error converting file to base64:', error);
+        // Handle error appropriately
+      }
+    );
+  }
+}
+
+convertFileToBase64(file: File): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      resolve(reader.result as string);
+    };
+
+    reader.onerror = (error) => {
+      reject(error);
+    };
+
+    reader.readAsDataURL(file);
+  });
+}
+
+
 
 }
