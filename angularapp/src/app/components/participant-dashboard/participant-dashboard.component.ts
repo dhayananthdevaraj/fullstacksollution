2ecaf8963b848c452a13321466c52472e100e020
neojarvis <techadmin@iamneo.ai>
2024-02-06 08:54:38 +0000

    Auto-save-commit-06/02/2024--14:24:36

--- angularapp/src/app/components/participant-dashboard/participant-dashboard.component.ts
+++ angularapp/src/app/components/participant-dashboard/participant-dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Event } from 'src/app/models/event.model';
+import { EventmanagementService } from 'src/app/services/eventmanagement.service';
 
 @Component({
   selector: 'app-participant-dashboard',
@@ -7,9 +10,80 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParticipantDashboardComponent implements OnInit {
 
-  constructor() { }
+  showDeletePopup = false;
+  selectedEvent: Event; 
+  showLogoutPopup = false;
+  selectedItem: any = {};
+  showModal: boolean = false;
+  
+  constructor(private router: Router,private eventManagementService: EventmanagementService ) { }
 
-  ngOnInit(): void {
+  navigateToAddRental() {
+    this.router.navigate(['/add-rental']);
   }
+  logout() {
+    // Perform logout logic here
+    // For example, clear user authentication, navigate to the login page, etc.
+    console.log('Logout clicked');
+    // For demonstration purposes, let's navigate to the login page
+    localStorage.removeItem('token');
+    localStorage.removeItem('userId');
+    localStorage.removeItem('role');
+    window.location.href = '/login';
+  }
+
+  events: any[] = []; // Declare the 'vacationRentals' property as an array of any type
+
+ngOnInit(): void {
+  // when add vacationRental button is clicked, trigger this function getBooksByUserId()
+  this.getAllEventManagement();
+}
+
+getAllEventManagement() {
+  this.eventManagementService.getAllEventManagement().subscribe(
+    (data) => {
+      console.log(data);
+      this.events = data;
+    },
+    (error) => {
+      console.error('Error retrieving vacationRentals', error);
+    }
+  );
+}
+
+viewInfo(vacationRental: any) {
+  this.selectedItem = vacationRental;
+  this.eventManagementService.getAllUsers().subscribe(users => {
+    const user = users.find(user => user.userId === this.selectedItem.userId);
+    if (user) {
+      this.selectedItem.firstName = user.firstName;
+      this.selectedItem.lastName = user.lastName;
+      this.selectedItem.email = user.email;
+      this.selectedItem.mobileNumber = user.mobileNumber;
+    }
+    console.log(this.selectedItem);
+  });
+  this.toggleModal();
+}
+
+toggleModal() {
+  this.showModal = !this.showModal;
+}
+
+searchText: string = ''; // Declare the 'searchText' property as a string
+sortValue: string = ''; // Declare the 'sortValue' property as a string
+searchVacationRentals() {
+  this.eventManagementService.searchEventManagements(this.searchText).subscribe(eventResponse => {
+    this.events = vacationRentals;
+  });
+}
+
+sortVacationRentals() {
+  this.eventManagementService.sortEventManagements(this.sortValue).subscribe(eventResponse => {
+    this.events = eventResponse;
+  });
+
+
+}
 
 }
