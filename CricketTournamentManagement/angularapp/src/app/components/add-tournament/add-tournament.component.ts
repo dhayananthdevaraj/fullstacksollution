bcf6d0f2af4cbef7bf612ba1fc3a2759dc80e8e1
neojarvis <techadmin@iamneo.ai>
2024-02-07 05:21:14 +0000

    Auto-save-commit-07/02/2024--10:51:11

--- CricketTournamentManagement/angularapp/src/app/components/add-tournament/add-tournament.component.ts
+++ CricketTournamentManagement/angularapp/src/app/components/add-tournament/add-tournament.component.ts
@@ -1,15 +1,101 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CrickettournamentService } from 'src/app/services/crickettournament.service';
 
 @Component({
   selector: 'app-add-tournament',
   templateUrl: './add-tournament.component.html',
   styleUrls: ['./add-tournament.component.css']
 })
-export class AddTournamentComponent implements OnInit {
+export class AddTournamentComponent {
 
-  constructor() { }
+  eventForm: FormGroup;
+  event: Event
+  photoImage="";
+  errorMessage = '';
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder, private cricketTournamentService: CrickettournamentService, private route:Router) {
+    this.eventForm = this.fb.group({
+      title: ['', Validators.required],
+      description: ['', Validators.required],
+      category: ['', Validators.required],
+      startDate: ['', Validators.required],
+      endDate: ['', Validators.required],
+      location: ['', Validators.required],
+      coverImage: [null, Validators.required],
+    });
+  }
+
+  categories = ['House', 'Apartment', 'Villa', 'Cabin', 'Condo', 'Other'];
+
+  onSubmit() {
+    if (this.eventForm.valid) {
+      // Call the service method to post the product
+      console.log(this.eventForm.value);
+      //pass userid from local storage
+      // this.eventForm.value.userId=localStorage.getItem('userId');
+      this.event = new Event();
+      this.event.title = this.eventForm.get('title').value;
+      this.event.category = this.eventForm.get('category').value;
+      this.event.description = this.eventForm.get('description').value;
+      this.event.startDate = this.eventForm.get('startDate').value;
+      this.event.location = this.eventForm.get('location').value;
+      this.event.endDate = this.eventForm.get('endDate').value;
+      this.event.coverImage = this.photoImage;
+      this.event.userId = localStorage.getItem('userId');
+      this.eventManagementService.addEventManagement(this.event).subscribe(
+        (response) => {
+          // Handle success if needed
+          console.log('event added successfully', response);
+          this.eventForm.reset(); // Reset the form
+          this.route.navigate(['/organiser-dashboard']);
+        },
+        (error) => {
+          // Handle error if needed
+          console.error('Error adding product', error);
+        }
+      );
+    }else{
+      this.errorMessage = "All fields are required"
+    }
+  }
+
+  handleFileChange(event: any): void {
+    const file = event.target.files[0];
+
+    if (file) {
+      this.convertFileToBase64(file).then(
+        (base64String) => {
+          this.photoImage=base64String
+        },
+        (error) => {
+          console.error('Error converting file to base64:', error);
+          // Handle error appropriately
+        }
+      );
+    }
+  }
+
+  convertFileToBase64(file: File): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        resolve(reader.result as string);
+      };
+
+      reader.onerror = (error) => {
+        reject(error);
+      };
+
+      reader.readAsDataURL(file);
+    });
+  }
+
+  goBack() {
+    // Navigate to the dashboard or any desired route
+    this.route.navigate(['/organiser-dashboard']);
   }
 
 }
